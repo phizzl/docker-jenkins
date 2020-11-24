13ed640f775119955d5dcbe0f9d6b9c10d3fb1ed
Phillip Schleicher <p.schleicher@nexus-united.com>
2020-11-24 08:55:23 +0100

    Modified CSP to allow JS on HTML reports

--- groovy/default-csp.groovy
+++ groovy/default-csp.groovy
@@ -1,2 +1,2 @@
 System.clearProperty("hudson.model.DirectoryBrowserSupport.CSP");
-System.setProperty("hudson.model.DirectoryBrowserSupport.CSP", "sandbox allow-same-origin allow-scripts; default-src 'self'; script-src * 'unsafe-eval'; img-src *; style-src * 'unsafe-inline'; font-src * data:");
+System.setProperty("hudson.model.DirectoryBrowserSupport.CSP", "sandbox allow-same-origin allow-scripts; default-src 'self'; script-src * 'unsafe-inline'; img-src *; style-src * 'unsafe-inline'; font-src * data:");
